e9414865de640ce0180b23442d9ca25b961d0c71
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-18 20:01:26 -0300

    style: strategies to get pixel perfect

--- app/components/reviews.tsx
+++ app/components/reviews.tsx
@@ -6,12 +6,14 @@ import { SvgBrandBbc } from "./svg-brand-bbc";
 import { SvgBrandLaweekly } from "./svg-brand-laweekly";
 import { SvgBrandNyt } from "./svg-brand-nyt";
 
+import "./review.css";
+
 /* components */
 import { SvgStarFive } from "./svg-star-five";
 
 export function Reviews() {
     return (
-        <div className="bg-gray-200 px-10 py-6 flex justify-between">
+        <div className="bg-gray-200 px-10 py-6 flex justify-between items-center">
             <div className="flex flex-col gap-3">
                 <span className="text-sm font-bold border border-black text-gray-800 w-fit h-fit p-3 rounded-lg">
                     #1 Doctor Recommended
@@ -25,7 +27,9 @@ export function Reviews() {
                     <span className="text-sm font-bold text-gray-800">12,000+ 5-star reviews</span>
                 </div>
             </div>
-            <MapBrands />
+            <div className="flex items-center gap-2">
+                <MapBrands />
+            </div>
         </div>
     );
 }
@@ -34,27 +38,27 @@ function MapBrands() {
     const brands = [
         {
             name: "Herb",
-            image: <ImageBrand image={herb} />,
+            image: <ImageBrand image={herb} width={40} />,
         },
         {
             name: "Men Journal",
-            image: <ImageBrand image={menJournal} />,
+            image: <ImageBrand image={menJournal} width={52} />,
         },
         {
             name: "Rolling Stone",
-            image: <ImageBrand image={rollingStone} />,
+            image: <ImageBrand image={rollingStone} width={90} />,
         },
         {
             name: "BBC",
-            image: <SvgBrandBbc />,
+            image: <div className="svg-container"><SvgBrandBbc /></div>,
         },
         {
             name: "LA Weekly",
-            image: <SvgBrandLaweekly />,
+            image: <div className="svg-container"><SvgBrandLaweekly /></div>,
         },
         {
             name: "NYT",
-            image: <SvgBrandNyt />,
+            image: <div className="svg-container"><SvgBrandNyt /></div>,
         },
     ];
     return brands.map((brand) => (
@@ -67,14 +71,14 @@ function MapBrands() {
 
 function WrapperBrand({ children }: { children: React.ReactNode }) {
     return (
-        <div className="bg-white rounded-lg flex items-center justify-center h-full">
+        <div className="bg-red-200 rounded-lg flex items-center justify-center w-32 h-10">
             {children}
         </div>
     );
 }
 
-function ImageBrand({ image }: { image: string }) {
+function ImageBrand({ image, width }: { image: string, width?: number }) {
     return (
-        <img src={image} alt="brand" className="w-full h-full object-cover" />
+        <img src={image} alt="brand" className="w-full h-auto object-contain" style={{ width: `${width}px` ?? "100%" }} />
     );
 }
