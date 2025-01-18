a005cfaea6789cf6f089fdda0d71fb514dcbb54d
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-18 03:14:46 -0300

    feat: update InfiniteMarquee component with new words and SVG integration
    - Refactored the InfiniteMarquee component to display a new set of words focused on quality and expertise.
    - Introduced a new Words function to manage the marquee content, incorporating SvgStar components for enhanced visual appeal.
    - Adjusted CSS for the marquee animation to ensure smooth scrolling and improved layout consistency.

--- app/components/infinite-marquee.tsx
+++ app/components/infinite-marquee.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { SvgStar } from "./svg-star";
 
 export function InfiniteMarquee() {
-    const words = ['Great', 'things', 'never', 'come', 'from', 'comfort', 'zones'];
-    const initialWords = words.concat(words, words, words, words);
-    const [text, setText] = useState(initialWords);
+
     return (
-        <div className="relative w-screen max-w-full overflow-hidden h-14">
-            <div className="absolute whitespace-nowrap will-change-transform animate-marquee flex flex-row gap-4 justify-between bg-black text-white items-center h-full">
-                {text.map((word, index) => (
-                    <span className="uppercase text-4xl font-bold" key={index}>{word}</span>
-                ))}
+        <div className="relative w-screen max-w-full overflow-hidden h-14 bg-black">
+            <div className="absolute whitespace-nowrap will-change-transform flex flex-row gap-4 justify-between text-white items-center h-full animate-marquee w-max pl-4">
+                <Words />
             </div>
         </div>
     )
+}
+
+function Words() {
+    const words = ['High Quality Ingredients', 'Independently Certified', 'Expert Driven', 'Shipped Internationally'];
+    const initialWords = [...words, ...words, ...words, ...words];
+
+    return initialWords.map((word, index) => {
+        if (index % 2 === 0) {
+            return <SvgStar key={index} />
+        } else if (index === initialWords.length - 1) {
+            return (
+                <>
+                    <span className="uppercase text-sm font-bold" key={index}>{word}</span>
+                    <SvgStar key={index + 1} />
+                </>
+            )
+        }
+        return <span className="uppercase text-sm font-bold" key={index}>{word}</span>
+    })
 }
