4425e2ff9950c052544c577d1ad01f0bdf448900
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-18 02:52:40 -0300

    feat: add multiple SVG components for enhanced UI
    - Introduced new SVG components including SvgArrowCircle, SvgArrow, SvgBrandBbc, SvgBrandLaweekly, SvgBrandNyt, SvgCart, SvgChat, SvgClipboard, SvgFb, SvgIg, SvgLike, SvgSearchGlass, SvgStar, SvgTw, SvgUserSuit, and SvgUser.
    - These components will be utilized throughout the application to improve visual consistency and user experience.
    - Each SVG component is designed to be reusable and customizable, enhancing the overall design of the application.

---
+++ app/components/svg-fb.tsx
@@ -0,0 +1,7 @@
+export function SvgFb() {
+    return (
+        <svg width="10" height="19" viewBox="0 0 10 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.95525 3.0544L8.22335 3.05555C6.8652 3.05555 6.60185 3.7007 6.60185 4.64715V6.73555H9.8414L9.41935 10.0073H6.60185V18.4H3.2243V10.0073H0.399902V6.73555H3.2243V4.324C3.2243 1.52375 4.93435 0 7.431 0C8.627 0 9.6551 0.08855 9.95525 0.1288V3.0544Z" fill="black" />
+        </svg>
+    )
+}
