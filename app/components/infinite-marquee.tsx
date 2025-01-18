5d57e57615b04c2e0ea18d02a96d08d54cc1c8bc
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-18 03:19:07 -0300

    refactor: update InfiniteMarquee component for improved word display and SVG integration
    - Adjusted the rendering logic in the Words function to enhance the display of words alongside SVG stars.
    - Removed redundant conditions to streamline the component's structure.
    - Maintained the marquee animation while ensuring a consistent layout for better visual appeal.

--- app/components/infinite-marquee.tsx
+++ app/components/infinite-marquee.tsx
@@ -4,7 +4,7 @@ export function InfiniteMarquee() {
 
     return (
         <div className="relative w-screen max-w-full overflow-hidden h-14 bg-black">
-            <div className="absolute whitespace-nowrap will-change-transform flex flex-row gap-4 justify-between text-white items-center h-full animate-marquee w-max pl-4">
+            <div className="absolute whitespace-nowrap will-change-transform flex flex-row gap-4 justify-between text-white items-center h-full w-max pl-4 animate-marquee">
                 <Words />
             </div>
         </div>
@@ -16,16 +16,19 @@ function Words() {
     const initialWords = [...words, ...words, ...words, ...words];
 
     return initialWords.map((word, index) => {
-        if (index % 2 === 0) {
-            return <SvgStar key={index} />
-        } else if (index === initialWords.length - 1) {
+        if (index === initialWords.length - 1) {
             return (
                 <>
                     <span className="uppercase text-sm font-bold" key={index}>{word}</span>
                     <SvgStar key={index + 1} />
                 </>
             )
+        } else {
+            return (
+                <>
+                    <SvgStar key={index + 1} />
+                    <span className="uppercase text-sm font-bold" key={index}>{word}</span>
+                </>)
         }
-        return <span className="uppercase text-sm font-bold" key={index}>{word}</span>
     })
 }
