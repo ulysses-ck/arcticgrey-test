3ebc04ec3ad4ed59f59fe9937b8cf0c19dd454f8
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-18 19:22:18 -0300

    feat: add SvgStarFive component for enhanced visual elements
    - Introduced a new SvgStarFive component that renders a five-pointed star SVG.
    - This component will be utilized to improve the visual appeal of the application and enhance user experience.

---
+++ app/components/svg-star-five.tsx
@@ -0,0 +1,9 @@
+export function SvgStarFive() {
+    return (
+        <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.0008 15.217L4.12295 18.5072L5.43573 11.9002L0.490234 7.32683L7.17943 6.53371L10.0008 0.416992L12.8222 6.53371L19.5113 7.32683L14.5659 11.9002L15.8787 18.5072L10.0008 15.217Z" fill="#F5BD41" />
+        </svg>
+
+    );
+}
+
