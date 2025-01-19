efba0d1d17679743c368562ae31271177b3699fd
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-19 20:41:44 -0300

    style: update SVG components to use camelCase attributes
    - Refactored multiple SVG components to replace hyphenated attributes (e.g., `clip-path`, `fill-rule`) with camelCase equivalents (e.g., `clipPath`, `fillRule`) for consistency with JSX standards.
    - This change enhances code readability and aligns with React's best practices for SVG rendering.

--- app/components/svg-yt.tsx
+++ app/components/svg-yt.tsx
@@ -2,7 +2,7 @@ export function SvgYt() {
     return (
         <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.955078" width="21.0286" height="15.7051" rx="3" fill="black" />
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M14.3386 8.11108C14.4828 8.18563 14.4828 8.39184 14.3386 8.46639L9.1328 11.158C8.99967 11.2269 8.84094 11.1302 8.84094 10.9804V5.5971C8.84094 5.44723 8.99967 5.35061 9.1328 5.41944L14.3386 8.11108Z" fill="white" />
+            <path fillRule="evenodd" clipRule="evenodd" d="M14.3386 8.11108C14.4828 8.18563 14.4828 8.39184 14.3386 8.46639L9.1328 11.158C8.99967 11.2269 8.84094 11.1302 8.84094 10.9804V5.5971C8.84094 5.44723 8.99967 5.35061 9.1328 5.41944L14.3386 8.11108Z" fill="white" />
         </svg>
     )
 }
