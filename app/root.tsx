0ee0af2765efd9a37e8eab707d245b8af327644a
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-19 22:38:01 -0300

    style: add Google Fonts preconnect and stylesheet links for improved typography

--- app/root.tsx
+++ app/root.tsx
@@ -140,6 +140,11 @@ export function Layout({children}: {children?: React.ReactNode}) {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" />
+        
       </head>
       <body>
         {data ? (
